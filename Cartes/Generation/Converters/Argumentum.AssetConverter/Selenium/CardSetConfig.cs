5a052845538644e70a27dd10d83b5dd7aa2e1de2
Jean-Sylvain Boige <jsboige@gmail.com>
2020-10-30 18:17:19 +0100

    Updated asset converter with additional margins
    Former-commit-id: cd2e6947dbfd723b9a1e67146d1b9e20a73719b1
    Former-commit-id: 7103ceb1a51b86d6f353afc8f26d16752ed5d88e

--- Cartes/Generation/Converters/Argumentum.AssetConverter/Selenium/CardSetConfig.cs
+++ Cartes/Generation/Converters/Argumentum.AssetConverter/Selenium/CardSetConfig.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic;
+using System.IO;
 using ImageMagick;
 
 namespace Argumentum.AssetConverter
 {
-    public class CardSetConfig
+
+    public class DocumentConfig
     {
 
-        public string Name { get; set; }
 
         public string DocumentName { get; set; }
+
+        public List<DocumentCardSet> CardSets { get; set; }
+
+    }
+
+
+    public class DocumentCardSet
+    {
+
+
+        public string CardSetName { get; set; }
+
+
+        public string TemplateDocumentName { get; set; }
 
         public int NbCopies { get; set; } = 1;
 
@@ -15,25 +31,40 @@ namespace Argumentum.AssetConverter
 
         public decimal WidthMM { get; set; }
 
-        public string TemplateDocumentName { get; set; }
-
-        public string FaceExampleName { get; set; }
-
-        public string BackExampleName { get; set; }
+        public decimal BorderMM { get; set; }
 
 
         public MagickImage LoadAndProcessImageUrl(string imageUrl)
         {
             var toReturn = ImageHelper.LoadImageFromEmbeddedUrl(imageUrl);
             ImageHelper.ConvertToCmyk(toReturn);
-            if (WidthMM>0 && HeigthMM>0)
+            if (WidthMM > 0 && HeigthMM > 0)
             {
-                ImageHelper.ResizeInMM(toReturn, WidthMM, HeigthMM);
+                ImageHelper.ResizeInMM(toReturn, WidthMM, HeigthMM, BorderMM);
             }
-            
+
             return toReturn;
         }
 
+    }
+
+    public class CardSetConfig
+    {
+
+        public string Name { get; set; }
+
+
+        public string FaceExampleName { get; set; }
+
+        public string BackExampleName { get; set; }
+
+
+        
+
+        public string GetSerializationName()
+        {
+            return Path.Combine(System.Environment.CurrentDirectory, $"{Name}-harvest.json");
+        }
 
     }
 }
