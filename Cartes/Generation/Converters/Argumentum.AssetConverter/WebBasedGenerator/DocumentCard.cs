479ed3f282348825bc52b4eb63eb9622081be96d
Jean-Sylvain Boige <jsboige@gmail.com>
2022-03-09 18:32:42 +0100

    Prise en compte retours imprimeur
    Former-commit-id: cf5c817062682cd6aea760ccfc7c9b2e0277e936
    Former-commit-id: e2794fc11390f552cb82f377027c552439576398

---
+++ Cartes/Generation/Converters/Argumentum.AssetConverter/WebBasedGenerator/DocumentCard.cs
@@ -0,0 +1,18 @@
+namespace Argumentum.AssetConverter
+{
+    public class DocumentCard
+    {
+
+        
+
+        public decimal HeigthMM { get; set; }
+
+        public decimal WidthMM { get; set; }
+
+
+        public decimal BorderMM { get; set; }
+
+
+
+    }
+}
