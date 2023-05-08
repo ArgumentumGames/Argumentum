92afb445d042b58232acf1e12891b81962583c87
Jean-Sylvain Boige <jsboige@gmail.com>
2023-05-08 11:10:38 +0200

    fix attempt for TypeConverter missing Assembly
    Former-commit-id: 8bcfa23f37807a85c50372d82966628d0dc2f160
    Former-commit-id: 62f9120dd747d4340f5c8cea160093c3e2507db6

--- Generation/Converters/Argumentum.AssetConverter/Mindmapper/MindMapCreatorConfig.cs
+++ Generation/Converters/Argumentum.AssetConverter/Mindmapper/MindMapCreatorConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -17,6 +18,11 @@ namespace Argumentum.AssetConverter.Mindmapper
     public class MindMapCreatorConfig
     {
 
+	     static MindMapCreatorConfig()
+	    {
+            //Attempt to fix missing Type/assembly.
+		    var tempConverter = new TypeConverter();
+	    }
 
         public MindMapCreatorConfig()
         {
